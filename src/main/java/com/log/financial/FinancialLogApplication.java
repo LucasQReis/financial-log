c372039ed9df09d9e626fc3038345479861108e5
LucasQReis <lqrreis@furb.br>
2025-04-22 08:23:14 -0300

    first commit

---
+++ src/main/java/com/log/financial/FinancialLogApplication.java
@@ -0,0 +1,13 @@
+package com.log.financial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialLogApplication.class, args);
+	}
+
+}
